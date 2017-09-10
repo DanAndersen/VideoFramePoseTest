6d9a52fb102be2994a00cc30d291a5168ddb8081
Andersen <andersed@purdue.lcl>
2017-09-10 14:07:14 -0400

    Add log viewer

---
+++ Assets/Scripts/DummyLogEmitter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Randomly prints debug strings. Used for testing that the Log Window is working correctly.
+/// </summary>
+public class DummyLogEmitter : MonoBehaviour {
+
+    public float MinSeconds = 1.0f;
+    public float MaxSeconds = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(EmitLogStrings());
+	}
+
+    private IEnumerator EmitLogStrings()
+    {
+        int messageNum = 0;
+        while(true)
+        {
+            float waitSeconds = UnityEngine.Random.Range(MinSeconds, MaxSeconds);
+            Debug.Log(String.Format("DummyLogEmitter: Test message {0}, waiting for {1} seconds...", messageNum, waitSeconds));
+            messageNum++;
+            yield return new WaitForSeconds(waitSeconds);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/HoloLogWindow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoloLogWindow : MonoBehaviour {
+
+    public int MaxNumMessages = 16;
+
+    public Text LogText;
+
+    private List<string> Messages = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+        Application.logMessageReceived += Application_logMessageReceived;
+
+        UpdateMessages();
+    }
+
+    private void Application_logMessageReceived(string condition, string stackTrace, LogType type)
+    {
+        Messages.Add(condition);
+        if (Messages.Count > MaxNumMessages)
+        {
+            Messages.RemoveAt(0);
+        }
+
+        UpdateMessages();
+    }
+
+    private void UpdateMessages()
+    {
+        string messagesString = "";
+        for (int i = 0; i < Messages.Count; i++)
+        {
+            messagesString += Messages[i] + "\n";
+        }
+
+        if (LogText != null)
+        {
+            LogText.text = messagesString;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
